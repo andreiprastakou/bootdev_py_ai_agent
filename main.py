4001fa4866f0f203a3e9573f795e54a2e8d8abe7
a-prostakov <andrei.prastakou@madnesspartners.com>
2025-11-25 23:48:55 +0100

    feat: pass input as prompt

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+
+if len(sys.argv) < 2:
+    print("Please provide a prompt")
+    sys.exit(1)
+
+prompt = sys.argv[1]
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,7 +15,7 @@ client = genai.Client(api_key=api_key)
 
 response =client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    contents=prompt
 )
 print(response.text)
 
