6be61db1b72e966ca2a1f1694d5b6c5bbfe37adb
a-prostakov <andrei.prastakou@madnesspartners.com>
2025-11-25 23:58:12 +0100

    feat: --verbose; multiple messages

--- main.py
+++ main.py
@@ -2,23 +2,32 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 if len(sys.argv) < 2:
     print("Please provide a prompt")
     sys.exit(1)
 
 prompt = sys.argv[1]
+verbose = "--verbose" in sys.argv
+
+if verbose:
+    print(f"User prompt: {prompt}")
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+]
 response =client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents=prompt
+    contents=messages
 )
 print(response.text)
 
 metadata = response.usage_metadata
-print(f"Prompt tokens: {metadata.prompt_token_count}")
-print(f"Response tokens: {metadata.candidates_token_count}")
+if verbose:
+    print(f"Prompt tokens: {metadata.prompt_token_count}")
+    print(f"Response tokens: {metadata.candidates_token_count}")
