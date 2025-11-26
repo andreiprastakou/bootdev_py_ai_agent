fb4e7797c40283c5a1a33eafb8f782622ddc18bb
a-prostakov <andrei.prastakou@madnesspartners.com>
2025-11-26 17:54:51 +0100

    feat: functions as tools (just info yet)

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions.index import available_functions
+
 if len(sys.argv) < 2:
     print("Please provide a prompt")
     sys.exit(1)
@@ -18,14 +20,26 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+system_prompt = """
+You are a helpful AI coding agent.
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+- List files and directories
+All paths you provide should be relative to the working directory. Do not specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
 messages = [
     types.Content(role="user", parts=[types.Part(text=prompt)]),
 ]
 response =client.models.generate_content(
     model="gemini-2.0-flash-001",
+    config=types.GenerateContentConfig(system_instruction=system_prompt, tools=[available_functions]),
     contents=messages
 )
-print(response.text)
+if response.function_calls != None:
+    for function_call in response.function_calls:
+        print(f"Calling function: {function_call.name}({function_call.args})")
+else:
+    print(response.text)
 
 metadata = response.usage_metadata
 if verbose:
